b5fc5a54b9b39b5c313ef43786fc5bd85e07e6e5
TunSawThant <thant.thura17101@gmail.com>
2019-11-16 17:56:55 +0630

    hello

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=E:\tunsawthant\november\nov_16_2019\Beautiful-List-UI-and-detail-page-master"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\src\flutter\bin\cache\artifacts\engine\ios"
